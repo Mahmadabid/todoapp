45578711fdbff0e4009c3f1d05da80b880c95e58
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-10 01:45:01 -0700

    updated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,9 @@ import store from './src/Global/store';
 import { Theme } from "./src/Global/theme";
 import './src/Global/Global.css';
 import { client } from "./src/Apollo/client";
-import { ApolloProvider } from '@apollo/client';
+
 
 export const wrapRootElement = ({ element }) => {
   
-  return <ApolloProvider client={client}><Provider store={store}><Theme>{element}</Theme></Provider></ApolloProvider>;
+  return <Provider store={store}><Theme>{element}</Theme></Provider>;
 };

7adbf9c86a689b4df5e8c02d55da4f8425fbc135
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-10 21:59:48 -0700

    send

--- functions/todolist.js
+++ functions/todolist.js
@@ -138,13 +138,6 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ context }) => {
-    if (context.clientContext.user) {
-      return { user: context.clientContext.user.sub };
-    } else {
-      return {};
-    }
-  },
 })
 
 const handler = server.createHandler()

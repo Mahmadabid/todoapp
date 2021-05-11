cbb88c8da9f4fa78d897e9ee6d6c9e4fbe5fd124
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-11 00:38:56 -0700

    Update client.js

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -23,7 +23,7 @@ const authLink = setContext((_, {headers}) => {
 
 const httpLink = new HttpLink({
   uri: "/.netlify/functions/todolist"
-  // uri: "https://ahm-todoapp.netlify.app/.netlify/functions/graphql"
+  // uri: "https://ahm-todoapp.netlify.app/.netlify/functions/todolist"
 });
 
 export const client = new ApolloClient({
